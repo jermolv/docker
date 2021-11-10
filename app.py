b32df0a15878d7ad9d2958a232b8dfe394d51739
jermolv <vasja.jermol@gmail.com>
2021-11-10 07:54:01 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'dela'
+    return 'dela lalalala'
 
 if __name__ == '__main__':
-    print('\nnub\n')
     app.run(debug=True, host='0.0.0.0')
